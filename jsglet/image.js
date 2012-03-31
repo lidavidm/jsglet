06e0f7121e814d6b00d7d94cc7434f9a7bfd81aa
David Li <li.davidm96@gmail.com>
2012-03-31 13:10:44 -0700

    Add image module

---
+++ jsglet/image.js
@@ -0,0 +1,45 @@
+define(["./common"], function(common) {
+
+    var Texture2D = Class.$extend({
+        __init__: function(gl, p_uniformLocation, p_browserImage) {
+            this._texture = gl.createTexture();
+            this._textureHandle = p_uniformLocation;
+            this.gl = gl;
+            gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
+            gl.bindTexture(gl.TEXTURE_2D, this._texture);
+            gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE,
+                          p_browserImage);
+            gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
+            gl.generateMipmap(gl.TEXTURE_2D);
+            gl.bindTexture(gl.TEXTURE_2D, null);
+        },
+
+        getTexture: function() {
+            return this;
+        },
+
+        bind: function() {
+            this.gl.activeTexture(this.gl.TEXTURE0);
+            this.gl.bindTexture(this.gl.TEXTURE_2D, this._texture);
+            this.gl.uniform1i(this._textureHandle, 0);
+        }
+    });
+
+    return {
+        Texture2D: Texture2D,
+
+        load: function(gl, p_uniformLocation, p_src) {
+            var image = new Image();
+            var deferred = new $.Deferred();
+            image.onload = function() {
+                var texture = new Texture2D(gl, p_uniformLocation, image);
+                deferred.resolve(texture);
+            }
+            image.src = p_src;
+            return deferred.promise();
+        }
+    }
+});
